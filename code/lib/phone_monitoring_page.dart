062945401d98f1fbb45751b6c29786f37d96f468
Gaven Pearl <127455079+xGOBx@users.noreply.github.com>
2024-09-30 16:23:52 -0400

    Battery Monitoring with alert
    	Can now view battery level
    	plays alert when battery is at
    	20%.login as Admin to test

--- code/lib/phone_monitoring_page.dart
+++ code/lib/phone_monitoring_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:battery_plus/battery_plus.dart'; 
+import 'package:battery_plus/battery_plus.dart';
+import 'package:audioplayers/audioplayers.dart'; 
+import 'package:firebase_auth/firebase_auth.dart'; 
 
 class PhoneMonitoringPage extends StatefulWidget {
   @override
@@ -7,13 +9,26 @@ class PhoneMonitoringPage extends StatefulWidget {
 }
 
 class _PhoneMonitoringPageState extends State<PhoneMonitoringPage> {
-  final Battery _battery = Battery();
+  final Battery _battery = Battery(); 
+  final AudioPlayer _audioPlayer = AudioPlayer();  
   int _batteryLevel = 100;
+  bool _playedWarning = false;
+  bool _isAdmin = false;  
 
   @override
   void initState() {
     super.initState();
+    _checkIfAdmin();
     _getBatteryLevel();
+  }
+
+  void _checkIfAdmin() {
+    User? user = FirebaseAuth.instance.currentUser;  
+    if (user != null && user.email == 'admin@example.com') {
+      setState(() {
+        _isAdmin = true;  
+      });
+    }
   }
 
   Future<void> _getBatteryLevel() async {
@@ -21,6 +36,22 @@ class _PhoneMonitoringPageState extends State<PhoneMonitoringPage> {
     setState(() {
       _batteryLevel = batteryLevel;
     });
+
+    if (_batteryLevel < 20 && !_playedWarning) {
+      _playWarningSound();
+      _playedWarning = true;
+    }
+  }
+
+  Future<void> _playWarningSound() async {
+    await _audioPlayer.setSourceAsset('sounds/battery_warning.mp3'); 
+    await _audioPlayer.resume(); 
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose(); 
+    super.dispose();
   }
 
   @override
@@ -41,6 +72,11 @@ class _PhoneMonitoringPageState extends State<PhoneMonitoringPage> {
               const Text(
                 'Warning: Battery is low!',
                 style: TextStyle(color: Colors.red, fontSize: 18),
+              ),
+            if (_isAdmin)
+              ElevatedButton(
+                onPressed: _playWarningSound,
+                child: const Text('Test Warning Sound'),
               ),
           ],
         ),
