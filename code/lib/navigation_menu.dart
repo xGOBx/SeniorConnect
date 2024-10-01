1ff1c788a15c5c8a348e5718668c7011a4ed9ccc
Gaven Pearl <127455079+xGOBx@users.noreply.github.com>
2024-10-01 14:56:07 -0400

    Map Function
    	Added a Map to the
    	near me page

--- code/lib/navigation_menu.dart
+++ code/lib/navigation_menu.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
-import 'emergency_help_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:battery_plus/battery_plus.dart'; 
 import 'meal_order_page.dart';
+import 'emergency_help_page.dart';
 import 'near_me_page.dart';
 import 'phone_monitoring_page.dart';
 import 'calendar_page.dart';
 import 'picture_page.dart';
+import 'auth_gate.dart';
 
-class NavigationMenu extends StatelessWidget {
+class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
 
   @override
+  _NavigationMenuState createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends State<NavigationMenu> {
+  final Battery _battery = Battery(); 
+  int _batteryLevel = 100; 
+
+  @override
+  void initState() {
+    super.initState();
+    _getBatteryLevel(); 
+  }
+
+  Future<void> _getBatteryLevel() async {
+    final int batteryLevel = await _battery.batteryLevel;
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final User? user = FirebaseAuth.instance.currentUser;
+
     return PopupMenuButton<String>(
-      onSelected: (value) {
+      onSelected: (value) async {
         switch (value) {
           case 'Meal Order':
             Navigator.push(context, MaterialPageRoute(builder: (context) => MealOrderPage()));
@@ -32,9 +58,29 @@ class NavigationMenu extends StatelessWidget {
           case 'Take Picture':
             Navigator.push(context, MaterialPageRoute(builder: (context) => PicturePage()));
             break;
+          case 'Logout':
+            await FirebaseAuth.instance.signOut();
+
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => const AuthGate()),
+            );
+            break;
         }
       },
       itemBuilder: (context) => [
+        PopupMenuItem(
+          enabled: false, 
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (user?.displayName != null) Text('Logged in as: ${user!.displayName}'),
+              if (user?.email != null) Text(user!.email!, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+              const SizedBox(height: 8),
+              Text('Battery Level: $_batteryLevel%', style: const TextStyle(fontSize: 12)),
+              const Divider(),
+            ],
+          ),
+        ),
         const PopupMenuItem(
           value: 'Meal Order',
           child: Text('Meal Order'),
@@ -58,6 +104,10 @@ class NavigationMenu extends StatelessWidget {
         const PopupMenuItem(
           value: 'Take Picture',
           child: Text('Take Picture'),
+        ),
+        const PopupMenuItem(
+          value: 'Logout',
+          child: Text('Logout', style: TextStyle(color: Colors.red)),
         ),
       ],
     );
